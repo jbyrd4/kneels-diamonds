88ab5950ce9289ad9caa5edbe7912e92ed6f07b9
jbyrd4 <jbyrd4@gmail.com>
2021-12-02 15:44:34 -0600

    book 4 completed through chapter 8, current chapter 9

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -1,29 +1,27 @@
 import { getSizes } from "./database.js"
+import { setSize } from "./database.js"
 
 const sizes = getSizes()
 
-document.addEventListener(
-    "change",
-    (event) => {
-        if (event.target.name === "size") {
-            window.alert(``)
-        }
-    }
-)
 
 export const DiamondSizes = () => {
     let html = "<ul>"
-
+    
     // Use .map() for converting objects to <li> elements
     const listItems = sizes.map(size => {
         return `<li>
-            <input type="radio" name="size" value="${size.id}" /> ${size.carets}
+        <input type="radio" name="size" value="${size.id}" /> ${size.carets}
         </li>`
     })
-
+    
     html += listItems.join("")
     html += "</ul>"
-
+    
     return html
 }
 
+document.addEventListener("change", (event) => {
+        if (event.target.name === "size") {
+            setSize(parseInt(event.target.value))
+        }
+    })
